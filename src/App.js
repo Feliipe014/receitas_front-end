d41cc0c8f4208d8ca396cf952ee1e097fa8df7fd
Felipe Nunes de Carvalho <06326904102@previpalmas.ti>
2023-04-28 17:26:21 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
+/* import axios from 'axios'; */
+
+import { useEffect, useState } from 'react';
+
 function App() {
+
+  const [receitas, setReceitas] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const res = await fetch('http://localhost:1337/api/receitas/')
+      const data = await res.json()
+      setReceitas(data.data)
+    }
+    fetchPosts()
+  },[]);
+
+  
+  console.log(receitas)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Receitas</h1>
+  
     </div>
   );
 }
